179d4667cea2a5db661346fb249e157f7e43dee1
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-04-30 11:18:27 +0530

    Update Ecommerce.sol

--- Ecommerce.sol
+++ Ecommerce.sol
@@ -12,9 +12,12 @@ contract Ecommerce {
         address payable seller;
         bool delivered;
     }
+    
+    //variables
     address payable public manager;
-    bool destroyer = false;
+    bool destroyer = false; 
 
+//constructor
     constructor() {
         manager = payable(msg.sender); // manager is set as payable, so he can send and receive ether
     }
@@ -24,15 +27,16 @@ contract Ecommerce {
     event bought(uint256 productId, address buyer);
     event delivered(uint256 productId);
 
-    uint256 counter = 1; // counter for increasaing the Productids
-    Product[] public products; // array of struct so we can track the products
+    uint256 counter = 1; // counter set to 1 for increasaing the Productids
+    Product[] public products; // empty array of struct so we can track the products
 
+//modifier
     modifier isNotdestroyed() {
         require(!destroyer, "contract does not exist"); // this modifier will help to know the whethever the contract is destroyed or not
         _;
     }
 
-    // function for registering products
+    // function for registering a products
     function registerProduct(
         string memory _title,
         string memory _description,
@@ -53,26 +57,26 @@ contract Ecommerce {
     // function to buy products
     function Buy(uint256 _productId) public payable isNotdestroyed {
         require(
-            products[_productId - 1].price == msg.value,
+            products[_productId - 1].price == msg.value, //_productId-1 because, we going to check our id 1 product on idex 0.
             "Pay the exact price"
         );
         require(
-            products[_productId - 1].seller != msg.sender,
+            products[_productId - 1].seller != msg.sender,// condintion for seller cant be the buyer
             "seller can't buy"
         );
-        products[_productId - 1].buyer = msg.sender;
+        products[_productId - 1].buyer = msg.sender;// one who calls the function wll be the buyer
         emit bought(_productId, msg.sender);
     }
 
     //to know the product has delivered or not
     function delivery(uint256 _productId) public {
         require(
-            products[_productId - 1].buyer == msg.sender,
+            products[_productId - 1].buyer == msg.sender,// only the buyer can confirm
             "only buyer can confirm"
         );
         products[_productId - 1].delivered = true;
         products[_productId - 1].seller.transfer(
-            products[_productId - 1].price
+            products[_productId - 1].price // as the user confim the product is deliverd, than only the exact price will be transfered to the seller
         );
         emit delivered(_productId);
     }
